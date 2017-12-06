183f7d4148c53c08f3f1b401d484d7e970e4087c
JustCallMeJeselle <32379688+JustCallMeJeselle@users.noreply.github.com>
2017-12-05 16:50:09 -1000

    Flappy Bird
    Video 4 5:00

--- assets/js/play.js
+++ assets/js/play.js
@@ -3,7 +3,17 @@
 var playState = {
 
     preload: function() { 
+        if (!game.device.desktop) {
+            game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+            game.scale.setMinMax(game.width/2, game.height/2, game.width, game.height);
+        }
         
+        game.scale.pageAlignHorizontally = true;
+        game.scale.pageAlignVertically = true;
+        
+        game.stage.backgroundColor = '#8185d5';
+        
+        game.load.image('bird', 'assets/images/banana.png');
     },
 
     create: function() { 
